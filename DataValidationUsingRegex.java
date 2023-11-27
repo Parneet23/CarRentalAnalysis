7c2817381110b7629b8e608fa8bb2e4f7b884cd3
pooja-vish <vishwak@uwindsor.ca>
2023-11-27 17:02:24 -0500

    DataValidator and PageRanking file added

--- DataValidationUsingRegex.java
+++ DataValidationUsingRegex.java
@@ -7,6 +7,15 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+package package1;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DataValidationUsingRegex {
 
 	// regex in mm/dd/yyyy format
@@ -16,35 +25,43 @@ public class DataValidationUsingRegex {
 	// Takes date input in a string as a parameter
 	// Returns true if date matches the pattern else false
 	public static boolean dataValidator(String date) {
-	    Pattern patternObject = Pattern.compile(DATE_FORMAT_REGEX);
-	    Matcher matcherObject = patternObject.matcher(date);
-	    return matcherObject.matches();
+		Pattern patternObject = Pattern.compile(DATE_FORMAT_REGEX);
+		Matcher matcherObject = patternObject.matcher(date);
+		return matcherObject.matches();
 	}
 
-	//main method
+	// main method
 	public static void dateValidation() throws DateException {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Enter your date here: ");
-            String inputDate = scanner.next();
-            try {
-                if (!dataValidator(inputDate)) {
-                    throw new DateException("Invalid Date Format");
-                }
+		try (Scanner scanner = new Scanner(System.in)) {
+			while (true) {
+				System.out.println("Enter your date here: ");
+				String inputStartDate = scanner.next();
+				System.out.println("Enter your  end date here: ");
+				String inputEndDate = scanner.next();
+				try {
+					if (!dataValidator(inputStartDate)) {
+						throw new DateException("Invalid Date Format");
+					}
 
-                SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
-                Date currentDate = new Date();
-                Date enteredDate = sdf.parse(inputDate);
+					SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+					Date currentDate = new Date();
+					Date startDate = sdf.parse(inputStartDate);
+					Date endDate = sdf.parse(inputEndDate);
 
-                if (enteredDate.before(currentDate)) {
-                    throw new DateException("Entered date should be greater than today's date");
-                }
+					if (startDate.before(currentDate) || endDate.before(currentDate)) {
+						throw new DateException("Entered date should be greater than today's date");
+					}
+					if (endDate.before(startDate) ) {
+						throw new DateException("Start date should be less than End date");
+					}
+					System.out.println("Successfully validated the date!!");
 
-                break; // Break out of the loop if a valid and future date is entered
-            } catch (DateException | ParseException e) {
-                System.out.println(e.getMessage());
-                // Continue the loop to prompt the user again
-            }
-        }
+					break; // Break out of the loop if a valid and future date is entered
+				} catch (DateException | ParseException e) {
+					System.out.println(e.getMessage());
+					// Continue the loop to prompt the user again
+				}
+			}
+		}
 	}
 }
---
+++ PageRanking.java
@@ -0,0 +1,108 @@
+package CarRent;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class PageRanking {
+	String websiteName;
+	Integer numberOfOccurance;
+
+	// parameterized constructor to initialize class variables
+	public PageRanking(String string1, Integer string2) {
+		this.websiteName = string1;
+		this.numberOfOccurance = string2;
+	}
+
+	// getters for class variables
+	public String getWebsiteName() {
+		return websiteName;
+	}
+
+	public Integer getNumberOfOccurance() {
+		return numberOfOccurance;
+	}
+
+	// method to divide the array into two parts
+	private static int splitArray(PageRanking[] arrayOfwebsites, int lefty, int righty) {
+
+		PageRanking pivotValue = arrayOfwebsites[righty];
+		int a = lefty - 1;
+
+		for (int b = lefty; b < righty; b++) {
+			// list.get(j).compareTo(pivot) < 0
+			if (arrayOfwebsites[b].compareTo(pivotValue) > 0) {
+				a = a + 1;
+
+				PageRanking temporary = arrayOfwebsites[a];
+				arrayOfwebsites[a] = arrayOfwebsites[b];
+				arrayOfwebsites[b] = temporary;
+			}
+		}
+		PageRanking temporary = arrayOfwebsites[a + 1];
+		arrayOfwebsites[a + 1] = arrayOfwebsites[righty];
+		arrayOfwebsites[righty] = temporary;
+
+		return a + 1;
+	}
+
+	// method to compare and return the class variable via which comparison is to be
+	// done
+	public int compareTo(PageRanking pr) {
+		return Integer.compare(this.numberOfOccurance, pr.numberOfOccurance);
+	}
+
+	// method to perform in-place quick sorting
+	private static void quickSorting(PageRanking[] arrayOfwebsites, int lefty, int righty) {
+		if (lefty < righty) {
+			int pivotPosition = splitArray(arrayOfwebsites, lefty, righty);
+			quickSorting(arrayOfwebsites, lefty, pivotPosition - 1);
+			quickSorting(arrayOfwebsites, pivotPosition + 1, righty);
+		}
+	}
+
+	// main method
+	public static void main(String[] args) {
+
+		int totalWebsites = 3, i = 0;
+		PageRanking[] pageRanker = new PageRanking[totalWebsites];
+
+		BufferedReader bReader;
+
+		try {
+			//get the websites name and their occuarances from the txt files
+			bReader = new BufferedReader(new FileReader("C:\\Users\\Dell\\eclipse-workspace\\ACCProject\\src\\package1\\count.txt"));
+			String line = bReader.readLine();
+			
+			//read the whole file line by line
+			while (line != null) {
+				String[] stringArray = new String[2];
+				line = line.replaceAll("\\s", "");
+				stringArray = line.split("=");
+				pageRanker[i] = new PageRanking(stringArray[0], Integer.parseInt(stringArray[1]));
+				line = bReader.readLine();
+				i++;
+			}
+			// close the reader object
+			bReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("BEFORE PAGE RANKING");
+		for (int j = 0; j < totalWebsites; j++) {
+			System.out.println("websiteName is " + pageRanker[j].websiteName + " and its number Of Occurance is "
+					+ pageRanker[j].numberOfOccurance);
+		}
+
+		// perform quick sorting to get the website names sorted according to the number of occurances
+		quickSorting(pageRanker, 0, pageRanker.length - 1);
+
+		System.out.println("AFTER PAGE RANKING");
+		for (int j = 0; j < totalWebsites; j++) {
+			System.out.println("websiteName is " + pageRanker[j].websiteName + " and its number Of Occurance is "
+					+ pageRanker[j].numberOfOccurance);
+		}
+	}
+
+}
