d1f56b09dda998c64d93aa25673bb2b82113d6b2
talhaseeb <talhahaseeb2@gmail.com>
2023-11-30 13:33:58 -0500

    Search count store in file changes

--- SearchCount.java
+++ SearchCount.java
@@ -1,31 +1,28 @@
-package CarRent;
+package carRentalAnalysis;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class SearchCount {
-
+	
+	private static final String FILE_PATH = "D:\\SEM1\\ACC\\project\\searchCount\\searchCount.txt";
     private Map<String, Integer> searchCountMap;
     //private Scanner scanner;
 
     // Constructor to initialize the SearchCount object
     public SearchCount() {
-        this.searchCountMap = new HashMap<>();
+        this.searchCountMap = loadSearchCountsFromFile();
         //this.scanner = new Scanner(System.in);
     }
 
     // Method to perform search counts
     public void performSearchCounts(String searchTerm) {
-        //while (true) {
-            // Ask the user for a search term
-//            System.out.print("Enter a search term (or 'exit' to end): ");
-//            String searchTerm = scanner.nextLine().toLowerCase();
-//
-//            // Check if the user wants to exit
-//            if (searchTerm.equals("exit")) {
-//                break;
-//            }
 
             // Update the search count
             int currentCount = searchCountMap.getOrDefault(searchTerm, 0);
@@ -43,11 +40,68 @@ public class SearchCount {
             System.out.println(entry.getKey() + ": " + entry.getValue() + " times");
         }
     }
+    private Map<String, Integer> loadSearchCountsFromFile() {
+        Map<String, Integer> loadedSearchCounts = new HashMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(":");
+                if (parts.length == 2) {
+                    String searchTerm = parts[0].trim();
+                    int count = Integer.parseInt(parts[1].trim());
+                    loadedSearchCounts.put(searchTerm, count);
+                }
+            }
+        } catch (IOException | NumberFormatException e) {
+            // Handle exceptions, or ignore if the file doesn't exist
+            e.printStackTrace();
+        }
+
+        return loadedSearchCounts;
+    }
+    
+    private void saveSearchCountsToFile() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH))) {
+            for (Map.Entry<String, Integer> entry : searchCountMap.entrySet()) {
+                writer.write(entry.getKey() + ": " + entry.getValue());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     // Method to close the Scanner
 //    public void closeScanner() {
 //        if (scanner != null) {
 //            scanner.close();
 //        }
+//    }
+    
+//    public void performSearchCounts() {
+//    	Scanner scanner  = new Scanner(System.in);
+//        while (true) {
+//            System.out.print("Enter a search term (or 'exit' to end): ");
+//            String searchTerm = scanner.nextLine().toLowerCase();
+//
+//            if (searchTerm.equals("exit")) {
+//                break;
+//            }
+//
+//            int currentCount = searchCountMap.getOrDefault(searchTerm, 0);
+//            searchCountMap.put(searchTerm, currentCount + 1);
+//
+//            System.out.println("Search count for '" + searchTerm + "': " + searchCountMap.get(searchTerm));
+//        }
+//
+//        saveSearchCountsToFile();
+//    }
+//    public static void main(String[] args) {
+//        SearchCount searchCount = new SearchCount();
+//
+//        searchCount.performSearchCounts();
+//        searchCount.displaySearchCountSummary();
+//        //searchCount.closeScanner();
 //    }
 }
